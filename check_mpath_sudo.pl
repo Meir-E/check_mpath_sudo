c1ecbf8cf0309f83922bdacb7f6a28a642b5f0f0
Meir-E <111319241+Meir-E@users.noreply.github.com>
2023-05-26 02:04:20 +0300

    Update and rename 20230525_2_check_mpath_sudo_2.pl to check_mpath_sudo.pl

--- check_mpath_sudo.pl
+++ check_mpath_sudo.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
-
+# Custom Script, based https://github.com/crphilipp/zabbix-agent-addons/blob/master/zabbix_scripts/check_mpath_sudo
+# By Meir-E
+# 
 use strict; # 
 use warnings; # 
 use JSON;
